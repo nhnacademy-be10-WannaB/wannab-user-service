1699ea305990778ecd555e1be4bf82ac0cca797b
gnsals0904 <gnsals0904@naver.com>
2025-06-30 15:27:58 +0900

    fix: secret key 값 static 필드 제거

--- src/main/java/shop/wannab/userservice/utils/Util.java
+++ src/main/java/shop/wannab/userservice/utils/Util.java
@@ -2,6 +2,7 @@ package shop.wannab.userservice.utils;
 
 
 import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
 import java.nio.charset.StandardCharsets;
 import java.security.Key;
 import org.springframework.beans.factory.annotation.Value;
@@ -10,8 +11,15 @@ public class Util {
     public final static String HEADER_ID_NAME = "X-USER-ID";
     public final static String HEADER_ROLE_NAME = "X-USER-ROLE";
 
-    @Value("${jwt.secret-key}")
     public static String SECRET;
-    public static final Key SECRET_KEY = Keys.hmacShaKeyFor(SECRET.getBytes(StandardCharsets.UTF_8));
+    public static Key SECRET_KEY;
 
+    @Value("${jwt.secret-key}")
+    public String secretValue;
+
+    @PostConstruct
+    public void init() {
+        SECRET = secretValue;
+        SECRET_KEY = Keys.hmacShaKeyFor(SECRET.getBytes(StandardCharsets.UTF_8));
+    }
 }
