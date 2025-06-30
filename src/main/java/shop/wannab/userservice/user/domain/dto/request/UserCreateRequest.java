09c721b0defe87ea9ab88fa651f44bacf2276890
gnsals0904 <gnsals0904@naver.com>
2025-06-30 14:44:35 +0900

    fix: password 정규표현식 검증 제거

--- src/main/java/shop/wannab/userservice/user/domain/dto/request/UserCreateRequest.java
+++ src/main/java/shop/wannab/userservice/user/domain/dto/request/UserCreateRequest.java
@@ -8,8 +8,7 @@ import jakarta.validation.constraints.Pattern;
 import java.time.LocalDate;
 
 public record UserCreateRequest(@NotBlank String username,
-                                // 비밀번호는 8~16자리, 특수문자, 숫자, 영어문자를 1글자 이상 포함해야 함
-                                @NotBlank @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,16}$") String password,
+                                @NotBlank String password,
                                 @NotBlank String name,
                                 @NotBlank @Email String email,
                                 // 전화번호는 11자리
