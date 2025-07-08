52fce36d36eddc8a03b65bcc781368f248374a63
yang-jaewon <rhddydapdlf01@gmail.com>
2025-07-08 17:18:00 +0900

    fix : 회원정보 수정 기능 회원정보 수정 api 반환 객체 수정

---
+++ src/main/java/shop/wannab/userservice/global/config/OAuth2Config.java
@@ -0,0 +1,14 @@
+//package shop.wannab.userservice.global.config;
+//
+//import feign.RequestInterceptor;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
+//
+//@Configuration
+//public class OAuth2Config {
+//    @Bean
+//    public RequestInterceptor oauth2FeignRequestInterceptor(OAuth2AuthorizedClientManager manager) {
+//        return new OAuth2FeignRequestInterceptor(manager, "payco");
+//    }
+//}
