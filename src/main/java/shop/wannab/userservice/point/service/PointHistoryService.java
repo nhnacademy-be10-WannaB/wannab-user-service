883e023e38f4289e93d91d5af1224cbd8f5cd2ec
jihoo <jihoo1214@gmail.com>
2025-07-11 13:30:29 +0900

    feat: 포인트 반환, 환불 api

--- src/main/java/shop/wannab/userservice/point/service/PointHistoryService.java
+++ src/main/java/shop/wannab/userservice/point/service/PointHistoryService.java
@@ -2,10 +2,17 @@ package shop.wannab.userservice.point.service;
 
 import java.util.List;
 import shop.wannab.userservice.point.domain.dto.PointHistoryCreateDTO;
+import shop.wannab.userservice.point.domain.dto.PointHistoryRollbackPointDTO;
 import shop.wannab.userservice.point.domain.entity.PointHistory;
 
 public interface PointHistoryService {
-    PointHistory createPointHistory(long userId, PointHistoryCreateDTO pointHistoryCreateDTO);
+    PointHistory createPointHistory(PointHistoryCreateDTO pointHistoryCreateDTO);
 
     List<PointHistory> readPointHistories(long userId);
+
+    void rollbackPointHistory(PointHistoryRollbackPointDTO pointHistoryRollbackPointDTO);
+
+    void cancel(Long orderId);
+
+    void refund(Long orderId, int amount);
 }
