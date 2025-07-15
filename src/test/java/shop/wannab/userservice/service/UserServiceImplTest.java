f8a94fa1f480fbf1f6aeedf20280e25cdcf6a3a0
jihoo <jihoo1214@gmail.com>
2025-07-15 17:04:57 +0900

    test: 테스트 코드 작성

--- src/test/java/shop/wannab/userservice/service/UserServiceImplTest.java
+++ src/test/java/shop/wannab/userservice/service/UserServiceImplTest.java
@@ -1,4 +1,4 @@
-package shop.wannab.userservice;
+package shop.wannab.userservice.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
