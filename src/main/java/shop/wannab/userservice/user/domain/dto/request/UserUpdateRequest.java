7b5547bcdc34132a4ec83ce95fe47c28847a44e1
jihoo <jihoo1214@gmail.com>
2025-07-17 09:39:51 +0900

    fix: valid 삭제

--- src/main/java/shop/wannab/userservice/user/domain/dto/request/UserUpdateRequest.java
+++ src/main/java/shop/wannab/userservice/user/domain/dto/request/UserUpdateRequest.java
@@ -5,7 +5,7 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 
 public record UserUpdateRequest(
-        @NotBlank @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,16}$") String password,
+        @NotBlank String password,
         @NotBlank String name,
         @NotBlank @Email String email,
         @NotBlank String nickname,
